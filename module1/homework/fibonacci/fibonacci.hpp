291cf85f34329824b9ebee289821b4c8a7adeb3c
kari0036 <kgebski68@gmail.com>
2020-05-15 19:54:12 +0200

    all works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence==0){
+        return 0;
+    }
+    if(sequence==1 || sequence==2){
+        return 1;
+    }
+    int last = 1, conteiner , score = 1; 
+    for(int i = 0;i<sequence-2;i++){
+        
+        conteiner=score;
+        score +=last;
+        last = conteiner;
+
+    }
+    return score;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if(sequence==0)return 0;
+    if(sequence <= 2)return 1;
+    return fibonacci_recursive(sequence-1)+fibonacci_recursive(sequence-2);
+
 }
